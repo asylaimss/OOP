9161f2f0dcb46f0480e45ab04d057737e10f29ca
asylaimss <157659038+asylaimss@users.noreply.github.com>
2025-03-07 00:45:06 +0500

    Delete lab5 directory

--- lab5/EmployeePractice/src/com/example/domain/Engineer.java
+++
@@ -1,7 +0,0 @@
-package com.example.domain;
-
-public class Engineer extends Employee {
-    public Engineer(int id, String name, String SSN, double salary) {
-        super(id, name, SSN, salary);
-    }
-}
